208a674184cc63f95dc029cbff6b8bd7464a282d
ShiningLight <shininglightapps@gmail.com>
2015-11-14 00:55:50 +0000

    Changed RequestQueue to use disk cache

--- app/src/main/java/com/almaghrib/mobile/RequestQueueSingleton.java
+++ app/src/main/java/com/almaghrib/mobile/RequestQueueSingleton.java
@@ -1,14 +1,30 @@
 package com.almaghrib.mobile;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.almaghrib.mobile.util.LruBitmapCache;
+import com.android.volley.Network;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HttpStack;
+import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.Volley;
 
+import java.io.File;
+
 public class RequestQueueSingleton {
+    private static final String TAG = "RequestQueueSingleton";
+
+    // Default maximum disk usage in bytes
+    private static final int DEFAULT_DISK_USAGE_BYTES = 25 * 1024 * 1024;
+
+    // Default cache folder name
+    private static final String DEFAULT_CACHE_DIR = "cache";
+
     private static RequestQueueSingleton mInstance;
     private static Context mContext;
 
@@ -34,7 +50,7 @@ public class RequestQueueSingleton {
         if (mRequestQueue == null) {
             // getApplicationContext() is key, it keeps you from leaking the
             // Activity or BroadcastReceiver if someone passes one in.
-            mRequestQueue = Volley.newRequestQueue(mContext.getApplicationContext());
+            mRequestQueue = newRequestQueue(mContext.getApplicationContext());
         }
         return mRequestQueue;
     }
@@ -51,5 +67,27 @@ public class RequestQueueSingleton {
 
     public ImageLoader getImageLoader() {
         return mImageLoader;
+    }
+
+    // Most code copied from "Volley.newRequestQueue(..)", we only changed cache directory
+    private static RequestQueue newRequestQueue(Context context) {
+        // define cache folder
+        File rootCache = context.getExternalCacheDir();
+        if (rootCache == null) {
+            Log.w(TAG, "Can't find External Cache Dir, "
+                    + "switching to application specific cache directory");
+            rootCache = context.getCacheDir();
+        }
+
+        final File cacheDir = new File(rootCache, DEFAULT_CACHE_DIR);
+        cacheDir.mkdirs();
+
+        final HttpStack stack = new HurlStack();
+        final Network network = new BasicNetwork(stack);
+        final DiskBasedCache diskBasedCache = new DiskBasedCache(cacheDir, DEFAULT_DISK_USAGE_BYTES);
+        final RequestQueue queue = new RequestQueue(diskBasedCache, network);
+        queue.start();
+
+        return queue;
     }
 }
